b9fabe933eec0227bbf15b4ffb7539b520f28e9e
Vladyslav <vladchershev@gmail.com>
2025-06-16 17:01:02 +0200

    HomeWork done

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+   
+    
+        Console.WriteLine("Enter the first number:");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number:");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        int result = number1 / number2;
+        Console.WriteLine($"The result of dividing {number1} by {number2} is: {result}");
+
+     
+    
+    
+    
